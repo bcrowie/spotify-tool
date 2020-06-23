30e6b9ac3b18c4163bc29ee8156920e347702872
Brenden Crowie <brendencrowie@gmail.com>
2020-06-23 17:20:49 -0400

    Created production build

---
+++ build/precache-manifest.ec1c2e034279063ec3d22acc90ca6061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33d4fc5e68bb47fffe303caa8571bd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1329cc75be9222530623",
+    "url": "/static/css/main.87d502de.chunk.css"
+  },
+  {
+    "revision": "8a3327b162a4e4678d6c",
+    "url": "/static/js/2.2500f5d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2500f5d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1329cc75be9222530623",
+    "url": "/static/js/main.7d6feab2.chunk.js"
+  },
+  {
+    "revision": "2ec0c0e5d66d66eef129",
+    "url": "/static/js/runtime-main.52320b52.js"
+  }
+]);
